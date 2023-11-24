a23a47743fbc165e83a2b00b61fdeb0b685c9a31
kalleolumets <olumetskalle@gmail.com>
2023-11-24 13:32:19 +0200

    clean code

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/register', 
+    [RegisteredUserController::class, 'store']);
